1e82ef5d2126955283a01f0342f0ce119594ba4a
Esat-Aydin <esat61_2001@hotmail.com>
2022-09-07 15:20:15 +0200

    mock employee data structure created

--- CRUDoperations/EmployeeData/IEmployeeData.cs
+++ CRUDoperations/EmployeeData/IEmployeeData.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using CRUDoperations.Models;
+using System;
+using System.Collections.Generic;
 
 namespace CRUDoperations.EmployeeData
 {
     public interface IEmployeeData
     {
         List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
     }
 }
---
+++ CRUDoperations/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CRUDoperations.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
