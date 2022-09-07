5297d82d6470fef65cdd811b633f931ff9166f18
Esat-Aydin <esat61_2001@hotmail.com>
2022-09-07 20:50:54 +0200

    employee context

--- CRUDoperations/Models/Employee.cs
+++ CRUDoperations/Models/Employee.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRUDoperations.Models
 {
     public class Employee
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters long")]
+
         public string Name { get; set; }
     }
 }
---
+++ CRUDoperations/Models/EmployeeContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDoperations.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        
+    }
+}
